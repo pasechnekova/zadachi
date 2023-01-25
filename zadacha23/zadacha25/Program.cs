b5557b4aae02ba46c27ca926a86c74d114b41270
Ирина Бакунович <pasechnekova@mail.ru>
2023-01-25 19:39:47 +0300

    Решение  25

--- zadacha23/zadacha25/Program.cs
+++ zadacha23/zadacha25/Program.cs
@@ -5,3 +5,22 @@
 
 Console.Clear();
 
+int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите число B: ");
+ToDegree(numberA, numberB);
+                          // Функция возведения в степень
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+                           // Функция ввода
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
