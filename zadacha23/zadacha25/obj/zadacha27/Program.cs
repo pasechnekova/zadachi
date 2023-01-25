15170f4094841b9b9562d4f3ddee401f91523872
Ирина Бакунович <pasechnekova@mail.ru>
2023-01-25 19:58:48 +0300

    Решение  27

--- zadacha23/zadacha25/obj/zadacha27/Program.cs
+++ zadacha23/zadacha25/obj/zadacha27/Program.cs
@@ -5,3 +5,30 @@
 
 Console.Clear();
 
+int number = ReadInt("Введите число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}       
+int NumberLen(int a) // Делаем подсчета цифр в числе
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index; 
+void SumNumbers(int n, int len) // вывода суммы цифр в числе
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
